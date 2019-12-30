c5fdec1c67d5ba676008177193a221cbc39ce867
guapacho <james.guapacho@gmail.com>
2019-12-30 08:03:23 -0500

    mi primer programa

---
+++ suma.py
@@ -0,0 +1,7 @@
+#sumar dos numeros por pantalla
+print ("sumar dos numeros")
+#iniciamos variables#
+numero1 = int(input ("introduzca el primer numero: "))
+numero2 = int(input ("introduzca el segundo numero: "))
+resultado = numero1 + numero2
+print (resultado)
